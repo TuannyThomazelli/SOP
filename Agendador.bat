2416d61653630cedad89a511f2a8c2f4d404b6bd
TuannyThomazelli <159180638+TuannyThomazelli@users.noreply.github.com>
2024-03-14 12:54:48 -0300

    Add files via upload

---
+++ Agendador.bat
@@ -0,0 +1,11 @@
+@echo off
+cls
+@echo.
+@echo ********************************
+@echo ***                          ***
+@echo ***   HORA DO ALMOCO :)      ***
+@echo ***                          ***
+@echo ********************************
+@echo
+@echo.
+pause
